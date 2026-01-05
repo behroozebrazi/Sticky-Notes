eaf17fc5d3507f2762d2bb6edb876c8e609f80e7
behroozebrazi <behber@berkeley.edu>
2026-01-04 16:05:40 -0800

    create note element - save and get notes from local storage

--- app.js
+++ app.js
@@ -2,4 +2,23 @@
 const noteContainer = document.querySelector('#app')
 const addNoteBtn = document.querySelector('.add-note')
 
+// Save notes in local storage
+function saveNotes(notes) {
+  localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// Load notes from local storage
+function getNotes() {
+  return JSON.parse(localStorage.getItem('notes') || '[]')
+}
+
+
+// Create a note from given data for DOM
+function createNoteElement(id, content) {
+  const noteElement = document.createElement('textarea')
+  noteElement.classList.add('note-base', 'note')
+  noteElement.id = id
+  noteElement.value = content
+  return noteElement
+}
 
